6368846e418066134c1d0423608a4e9db7df55d5
NAVEENA M G <79916384+myselfnaveenamg@users.noreply.github.com>
2023-02-14 00:56:08 +0530

    Add files via upload

---
+++ Abstraction/Abstract Class/Bank.java
@@ -0,0 +1,15 @@
+abstract class Bank{    
+    abstract int getRateOfInterest();    
+}   
+class SBI extends Bank{    
+    int getRateOfInterest(){
+        return 7;
+    }    
+}    
+class PNB extends Bank{    
+    int getRateOfInterest(){
+        return 8;
+    }    
+}    
+    
+  
---
+++ Abstraction/Interface/TestInterfaceStatic.java
@@ -0,0 +1,25 @@
+package Abstraction.Interface;
+
+interface Drawable{  
+    void draw();  
+    static int cube(int x){
+        return x*x*x;
+    }  
+}  
+    
+class Rectangle implements Drawable{  
+    public void draw(){
+        System.out.println("drawing rectangle");
+    }  
+}  
+    
+class TestInterfaceStatic{  
+    public static void main(String args[]){  
+        Drawable d=new Rectangle();  
+        d.draw();  
+        System.out.println(Drawable.cube(3)); 
+        System.out.println(Drawable.cube(4));
+        System.out.println(Drawable.cube(5));
+        System.out.println(Drawable.cube(6)); 
+    }
+} 
