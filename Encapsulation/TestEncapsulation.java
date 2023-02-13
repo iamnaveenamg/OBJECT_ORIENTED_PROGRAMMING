11646387a22f92f24d231a0c16fda8ed5f36a4f7
NAVEENA M G <79916384+myselfnaveenamg@users.noreply.github.com>
2023-02-14 00:59:42 +0530

    Update TestEncapsulation.java

--- Encapsulation/TestEncapsulation.java
+++ Encapsulation/TestEncapsulation.java
@@ -8,8 +8,8 @@ public static void main(String[] args) {
     Account acc=new Account();  
     //setting values through setter methods  
     acc.setAcc_no(7560504000L);  
-    acc.setName("Sonoo Jaiswal");  
-    acc.setEmail("sonoojaiswal@javatpoint.com");  
+    acc.setName("Naveen");  
+    acc.setEmail("Naveen.Reddy@gmail.com");  
     acc.setAmount(500000f);  
     //getting values through getter methods  
     System.out.println("account No:"+acc.getAcc_no()+" Name: "+acc.getName()+" Email ID:  "+acc.getEmail()+" Amount: "+acc.getAmount());  
